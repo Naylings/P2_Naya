34fa8a012c1eb0608f895ba0cc3c4fe4b103fbe2
Naylings <mnayadz168@gmail.com>
2026-01-26 00:27:58 +0700

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
---
+++ vite.config.js
@@ -0,0 +1,38 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import laravel from "laravel-vite-plugin";
+import Components from "unplugin-vue-components/vite";
+import { PrimeVueResolver } from "unplugin-vue-components/resolvers";
+import tailwindcss from "@tailwindcss/vite";
+
+
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+        Components({
+        resolvers: [PrimeVueResolver()],
+        dts: "resources/js/components.d.ts", // optional
+    }),
+    laravel({
+      input: ["resources/js/app.js"],
+      refresh: true,
+    }),
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      "@": "/resources/js",
+    },
+  },
+  optimizeDeps: {
+    noDiscovery: true,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
+});
